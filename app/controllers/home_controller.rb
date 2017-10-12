9881148938019959890be24b32b15fd974743c46
Waqar Hassan <waqar.hassan@virtualforce.io>
2017-10-12 18:58:49 +0500

    first crawler flow set up before mechanize integration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
+	def crawl 
+		@jobs = []
+		respond_to do |format|
+	      format.js { render :partial => "crawl" }
+	    end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
+  match "/home/crawl",  via: [ :post] , as: :crawl
 end
