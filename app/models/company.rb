4c708174a4aa03662b2d5e824ff4c598b053f155
Waqar Hassan <waqar.hassan@virtualforce.io>
2017-10-25 10:26:28 +0500

    refactoring

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,12 +5,10 @@ class Company < ApplicationRecord
   def create_domain_search
 
     access_token = Snovio.get_access_token
-    puts "--------------------#{self.inspect}------------------#{self.domain_search.inspect}-----------------"
-    self.domain_search = DomainSearch.new(domain: self.trim_web_address)
-    domain_search = self.domain_search #.present? ? self. find_or_create_by!(domain: self.trim_web_address)
+    self.domain_search = DomainSearch.new(domain: self.trim_web_address) #find_or_initialize_by here WAQAR
+    domain_search = self.domain_search
     @emails = Snovio.get_emails domain_search.domain , access_token
-    # @domain_search = @company.domain_search.find_or_create_by!(domain: web_address)
-
+    @domain_search = @company.domain_search.find_or_create_by!(domain: web_address)
     @emails["emails"].each do |email|
       	domain_search.emails.find_or_create_by(email: email["email"]) do |email_row|
       		email_row.address_type = email["type"]
