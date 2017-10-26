1821706f23f12bac53e3c6cb566c5e4eaa318122
Waqar Hassan <waqar.hassan@virtualforce.io>
2017-10-26 18:27:24 +0500

    js test

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,9 +1,9 @@
+console.log("Custom js is working")
+alert("000000")
 function show_waiting_bar(){
     $("#waiting_bar").modal("show")
 }
 function get_batch (obj){
     id = $(obj).val()
-    // alert("Yalla Habibi" + id)
     window.top.location.href = "/"+id
-
 }
