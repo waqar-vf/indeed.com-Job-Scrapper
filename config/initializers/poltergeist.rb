25c4173cacce643c02cb942c7fe4a0e90c225a7d
Waqar Hassan <waqar.hassan@virtualforce.io>
2017-10-26 16:36:24 +0500

    req capybara

--- config/initializers/poltergeist.rb
+++ config/initializers/poltergeist.rb
@@ -1,3 +1,4 @@
+require 'capybara'
 Capybara.javascript_driver = :poltergeist
   options = { js_errors: false, js: true , timeout: 60, phantomjs_logger: StringIO.new, logger: nil, :phantomjs_options => ['--debug=no', '--ignore-ssl-errors=yes', '--ssl-protocol=TLSv1','--load-images=no',"--cookies-file=cookies.txt"], :debug => false }
   Capybara.register_driver :poltergeist do |app|
