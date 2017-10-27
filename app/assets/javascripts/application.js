c1e781123182971f81977c03c17780381d83b89d
Waqar Hassan <waqar.hassan@virtualforce.io>
2017-10-27 16:18:28 +0500

    js is app.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,19 @@
 //= require_tree .
 
 console.log("WORKING")
+
+function show_waiting_bar(){
+    $("#waiting_bar").modal("show")
+}
+function get_batch (obj){
+    id = $(obj).val()
+    window.top.location.href = "/"+id
+}
+function something_went_wrong() {
+    alert('Something went wrong. Please try again later');
+    $("#waiting_bar").modal("hide")
+}
+function get_all_emails_success() {
+    alert("All emails for the current active batch are in the system. You can download csv now.")
+    $("#waiting_bar").modal("hide")
+}
