b4c9978d8e14453253b63ff27181f0f00f7a10b4
Waqar Hassan <waqar.hassan@virtualforce.io>
2017-10-30 10:24:52 +0500

    indeed us

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   def job_site_url
-  	"https://www.indeed.com.pk"
+  	"https://www.indeed.com"
   end
   def search_engine_url
   	"http://www.google.com"
